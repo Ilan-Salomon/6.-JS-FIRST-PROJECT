5993a192b4bc31075556debd2b37309c70832de9
Ilan Salomon <ilansalomon@gmail.com>
2025-02-03 17:59:58 -0300

    added a new button and its fucntionality

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-let pressButtonChangeColor  = document.body.querySelector('button');
+let pressButtonChangeColor  = document.getElementById('button1');
+let pressButtonAddLI  = document.getElementById('button2');
 
 function changeColorFunc(){
-    let h1ToTurnRed = document.body.querySelector("h1");
-    h1ToTurnRed.style.color = "red"
-
+    let h1 = document.body.querySelector("h1");
+    h1.style.color = "red"
+    h1.style.margin = 100
+    console.log("addMargin")
 };
 
+function addRandomListItem(){
+    let ul = document.getElementById('list1');
+    let li = document.createElement("li");
+    let children = ul.children.length + 1;
+    li.setAttribute("id", "element"+children)
+    li.appendChild(document.createTextNode("Element "+children));
+    ul.appendChild(li)
+    console.log("testAppendChild")
+};
+
+
+
+
 pressButtonChangeColor.onclick = changeColorFunc; 
+pressButtonAddLI.onclick = addRandomListItem; 
 
 
